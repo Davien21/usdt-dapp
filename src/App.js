24aca0817c91111942cbc69791b3399c525be101
davien21 <chidiebereekennia@gmail.com>
2021-12-09 17:35:22 +0100

    --feat: first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router } from "react-router-dom";
 
+import AppRouter from "./appRouter";
+import { Toast, Loader } from "./components";
+import { AppProvider } from "./contexts/appContext";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router basename={"/"}>
+        <AppProvider>
+          <Loader />
+          <Toast />
+          <AppRouter />
+        </AppProvider>
+      </Router>
     </div>
   );
 }
