4c850f0f1a8e46cadac4b4a32eadfb30c57a286e
davien21 <chidiebereekennia@gmail.com>
2021-12-09 18:18:14 +0100

    --refactor: minor css updates

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -12,22 +12,22 @@ function Home() {
     <div className={`${styles["container"]}`}>
       <Header />
       <main className={`pb-5`}>
-        <div className="bg-white py-4 mb-5">
+        <div className="bg-white pt-4 mb-5">
           <div className="container">
             <div className="row justify-content-between">
-              <div className={`${styles["info-card"]} col-auto`}>
+              <div className={`${styles["info-card"]} col-auto mb-4`}>
                 <span className="mb-1">Initial Supply</span>
                 <span>{formatMoney(USDTData.initialSupply)}</span>
               </div>
-              <div className={`${styles["info-card"]} col-auto`}>
+              <div className={`${styles["info-card"]} col-auto mb-4`}>
                 <span className="mb-1">Total Supply</span>
                 <span>{formatMoney(USDTData?.totalSupply)}</span>
               </div>
-              <div className={`${styles["info-card"]} col-auto`}>
+              <div className={`${styles["info-card"]} col-auto mb-4`}>
                 <span className="mb-1">Wallet Balance</span>
                 <span>{formatMoney(USDTData?.balance)}</span>
               </div>
-              <div className={`${styles["info-card"]} col-auto`}>
+              <div className={`${styles["info-card"]} col-auto mb-4`}>
                 <span className="mb-1">Symbol</span>
                 <span>{USDTData?.symbol}</span>
               </div>
@@ -37,9 +37,9 @@ function Home() {
         <div className="">
           <div
             id={`${styles["recent-transactions"]}`}
-            className="bg-white px-0 container"
+            className="bg-white px-0 container-md overflow-auto"
           >
-            <div className="pt-3 px-4">
+            <div className="pt-4 px-4">
               <div
                 className={`row justify-content-between mb-3 ${styles["contract-name"]}`}
               >
@@ -47,9 +47,7 @@ function Home() {
                 <span className="col-auto">{USDTData?.name}</span>
               </div>
               <div className="row justify-content-between align-items-center mb-4">
-                <p
-                  className={`${styles["title"]} col-auto mb-0 text-center mx-auto`}
-                >
+                <p className={`${styles["title"]} col-auto mb-0 mx-md-auto`}>
                   Recent Transactions
                 </p>
               </div>
@@ -60,14 +58,21 @@ function Home() {
               </div>
             </div>
             <hr className="mt-" />
-            <div className="pb-3 px-4">
+            <div className={`pb-4 px-4 ${styles["transactions"]}`}>
               {transactions.map((item, index) => (
                 <div key={index + item.amount} className="row ">
                   <div className="mb-2 col-4">
-                    Sent <span className={`${styles["green-text"]}`}>{formatMoney(item.amount)} USDT</span>
+                    <span className="mr-2">Sent</span>
+                    <span className={`${styles["green-text"]}`}>
+                      {formatMoney(item.amount)} USDT
+                    </span>
                   </div>
-                  <div className="mb-2 col-4">{formatAddress(item.from)}</div>
-                  <div className="mb-2 col-4">{formatAddress(item.to)}</div>
+                  <div className="mb-2 col-4" title={`${item.from}`}>
+                    {formatAddress(item.from)}
+                  </div>
+                  <div className="mb-2 col-4" title={`${item.to}`}>
+                    {formatAddress(item.to)}
+                  </div>
                 </div>
               ))}
             </div>
